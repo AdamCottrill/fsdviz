6fc55db45fcfc46566aa3c595ae03abde7284cfe
Adam Cottrill <adam.cottrill@ontario.ca>
2019-03-29 11:42:37 -0400

    Update map to zoom to points and populate summary table.
    The mapbox map will now zoom to show all of the selected points or the
    extent of the selected management unit (whichever is greater).  Some
    functions were added to update the stats panel with summary statistics
    - total number of events, species and fish stocked, and a table by species.

--- fsdviz/static/js/stocking_event_list_map.js
+++ fsdviz/static/js/stocking_event_list_map.js
@@ -1,6 +1,25 @@
 
 /* global d3, mapboxgl */
 
+// hard code for now - this is the bounding box for Lake Huron as returned by postgres
+// TODO: make dynamic based on url (lake, juristiction,  region of interest etc)
+
+
+pgbbox = "POLYGON((-84.8052136556106 42.9479243183533,-84.8052136556106 46.5789677934838,-79.6071771544244 46.5789677934838,-79.6071771544244 42.9479243183533,-84.8052136556106 42.9479243183533))";
+
+const pgbbox_corners = (pgbbox) => {
+    // get the coordinates of each corner from the string:
+    const corners = pgbbox.slice(pgbbox.indexOf('((') + 2, pgbbox.indexOf('))')).split(',');
+    // mapbox requires the south west and north east corners (first
+    // and third elements) as floating points numbers:
+    const sw_corner = corners[0].split(" ").map(d=> +d);
+    const ne_corner = corners[2].split(" ").map(d=> +d);
+
+    return [sw_corner, ne_corner];
+}
+
+
+let mapBounds = pgbbox_corners(pgbbox);
 
 mapboxgl.accessToken = "pk.eyJ1IjoiYWNvdHRyaWxsIiwiYSI6ImNpazVmb3Q2eDAwMWZpZm0yZTQ1cjF3NTkifQ.Pb1wCYs0lKgjnTGz43DjVQ";
 
@@ -8,8 +27,9 @@ mapboxgl.accessToken = "pk.eyJ1IjoiYWNvdHRyaWxsIiwiYSI6ImNpazVmb3Q2eDAwMWZpZm0yZ
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-81.857221, 45.194331],
-    zoom: 7,
+    //center: [-81.857221, 45.194331],
+    //zoom: 7,
+    bounds: mapBounds
 
 });
 
@@ -59,6 +79,88 @@ const mapboxProjection = (lonlat) => {
 let  d3Projection = getD3('map');
 
 
+
+const update_map_bounds = (pts) => {
+    //get the extends of our points and make sure reset the zoom of
+    //our map to ensure that no points are missed at the orginal zoom:
+    const max_lon = d3.max(pts, d=>d.value.coordinates[0]);
+    const max_lat = d3.max(pts, d=>d.value.coordinates[1]);
+    const min_lon = d3.min(pts, d=>d.value.coordinates[0]);
+    const min_lat = d3.min(pts, d=>d.value.coordinates[1]);
+
+    let sw_corner = mapBounds[0];
+    let ne_corner = mapBounds[1];
+
+    sw_corner[0]  = sw_corner[0] < min_lon ? min_lon : sw_corner[0];
+    sw_corner[1]  = sw_corner[1] < min_lat ? min_lat : sw_corner[1];
+
+    ne_corner[0]  = ne_corner[0] > max_lon ? max_lon : ne_corner[0];
+    ne_corner[1]  = ne_corner[1] > max_lat ? max_lat : ne_corner[1];
+
+    map.fitBounds([sw_corner, ne_corner]);
+
+}
+
+
+
+const update_summary_table = data => {
+    // generate the html for rows of our summary table body.  for each species in data
+    // we want to generate html that looks like this:
+    //   <tr>
+    //       <td>${ row.common_name.title }</td>
+    //       <td>${ row.event_count }</td>
+    //       <td>${ commaFormat(row.total_stocked) }</td>
+    //   </tr>
+
+    let commaFormat = d3.format(',d');
+    html = "";
+
+    data.forEach(row => {
+
+        html += `<tr>
+           <td>${ row.key }</td>
+           <td>${ row.value.events }</td>
+           <td>${ commaFormat(row.value.total) }</td>
+       </tr>`;
+    });
+
+    d3.select("#stocked-summary-table-tbody").html(html);
+
+}
+
+
+
+const update_stats_panel = data =>{
+
+    let byspecies = d3.nest()
+        .key(d =>  d.species.common_name )
+        .rollup(values => { return {
+            total: d3.sum(values, d => +d.yreq_stocked ),
+            events: values.length,
+        };
+                          })
+        .entries(data);
+
+    // sort out table by total number stocked:
+    byspecies.sort((a,b) => b.value.total - a.value.total);
+
+    let species_count = byspecies.length;
+    let event_count = d3.sum(byspecies, d=>d.value.events);
+    let total_stocked = d3.sum(byspecies, d=>d.value.total);
+
+    let commaFormat = d3.format(',d');
+
+    d3.selectAll("#species-count").text(commaFormat(species_count));
+    d3.selectAll("#event-count").text(commaFormat(event_count));
+    d3.selectAll("#total-stocked").text(commaFormat(total_stocked));
+
+    update_summary_table(byspecies);
+
+
+}
+
+
+
 // by convention - our api urls will be the same as our view urls
 // except they will include the api and version number
 
@@ -66,6 +168,8 @@ let api_url = window.location.href
     .replace(window.location.host, window.location.host + '/api/v1');
 
 d3.json(api_url).then((data) => {
+
+    update_stats_panel(data);
 
     // now aggregate by point and species - how many events, now many fish.
 
@@ -84,6 +188,9 @@ d3.json(api_url).then((data) => {
         .entries(data);
 
     pts.forEach(d=> d.value.coordinates=get_coordinates(d.key));
+
+    update_map_bounds(pts);
+
 
 
     let  dots = svg.selectAll("circle.dot")
