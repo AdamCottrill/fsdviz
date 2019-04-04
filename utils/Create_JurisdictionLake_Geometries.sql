3aca2b014745a66f861da139d4a560667ceb9830
Adam Cottrill <adam.cottrill@ontario.ca>
2019-04-04 16:00:28 -0400

    Utilities to import management unit geometries from shapefiles
    This commit includes a python file that processes and imports
    shapefiles of management units for all 5 lakes. It also includes a sql
    script that creates jurisdictions and lake polygons from associted
    management areas.  The requirements file has been updated to include
    pyshape - an additional library required to read shapefiles.

---
+++ utils/Create_JurisdictionLake_Geometries.sql
@@ -0,0 +1,189 @@
+-- ==========================================================
+--                  AGGREGEATE MANAGEMENT UNITS
+-- these are management units in the GLFSDB that are amalgamations of
+-- other management units:
+--update common_managementunit set label = 'IL' where label='ILL';
+--update common_managementunit set label = 'IN' where label='IND';
+-- make MM123
+UPDATE common_managementunit
+   SET geom = (SELECT st_multi(st_union (geom)) AS geom
+               FROM common_managementunit AS mu2
+               WHERE mu2.label IN ('MM1','MM2','MM3'))
+WHERE label = 'MM123';
+
+-- make Wisconsin waters of Lake Michigan - it is currently both a
+-- mangement unit and a juristiction. This is the mangament unit:
+UPDATE common_managementunit
+   SET geom = (SELECT st_multi(st_union (geom)) AS geom
+               FROM common_managementunit AS mu2
+               WHERE mu2.label LIKE 'WM%')
+WHERE label = 'WM';
+
+--update the centroid fo each management unit:
+UPDATE common_managementunit
+   SET centroid = st_centroid(geom)
+WHERE geom IS NOT NULL;
+
+-- ==========================================================
+--                  JURISDICTIONS
+
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label = 'SC_ON')
+WHERE slug = 'sc_on';
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label = 'SC_MI')
+WHERE slug = 'sc_mi';
+
+
+-- LAKE Ontario - ontario waters:
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'OO%')
+WHERE name = 'Ontario-Ontario';
+
+-- LAKE Ontario - New York waters:
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'NO%')
+WHERE name = 'Ontario-New York';
+
+COMMIT;
+
+-- LAKE Erie - ontario waters:
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'OE%')
+WHERE name = 'Erie-Ontario';
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label IN ('O1','O2','O3'))
+WHERE name = 'Erie-Ohio';
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label = 'MICH')
+WHERE name = 'Erie-Michigan';
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label = 'NY')
+WHERE name = 'Erie-New York';
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label = 'PENN')
+WHERE name = 'Erie-Pennsylvania';
+
+COMMIT;
+
+-- LAKE Superior - ontario waters:
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'OS%')
+WHERE name = 'Superior-Ontario';
+
+-- LAKE Superior - Michigan waters:
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'MS%')
+WHERE name = 'Superior-Michigan';
+
+-- LAKE Superior - Minnesota waters:
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label IN ('M1','M2','M3'))
+WHERE name = 'Superior-Minnesota';
+
+-- LAKE Superior - Wisconsin waters:
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label = 'WISC')
+WHERE name = 'Superior-Wisconsin';
+
+COMMIT;
+
+-- LAKE HURON - michigan waters
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'MH%')
+WHERE name = 'Huron-Michigan';
+
+-- LAKE HURON - ontario waters:
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'OH%'
+                    OR    mu2.label LIKE 'NC%'
+                    OR    mu2.label LIKE 'GB%')
+WHERE name = 'Huron-Ontario';
+
+COMMIT;
+
+-- LAKE MICHIGAN
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'WM%')
+WHERE name = 'Michigan-Wisconsin';
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'MM%')
+WHERE name = 'Michigan-Michigan';
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'IL%')
+WHERE name = 'Michigan-Illinois';
+
+UPDATE common_jurisdiction
+   SET shoreline = (SELECT st_multi(st_union (geom)) AS geom
+                    FROM common_managementunit AS mu2
+                    WHERE mu2.label LIKE 'IN%')
+WHERE name = 'Michigan-Indiana';
+
+-- update the extents of each juristiction with its bountding box:
+UPDATE common_jurisdiction
+   SET extents = st_multi(st_envelope (shoreline));
+
+COMMIT;
+
+---===============================
+--         LAKES
+-- update the shorelines for each lake by merging all of the juristictions in each lake
+UPDATE common_lake
+   SET shoreline = shoreline.geom
+FROM (SELECT lake.abbrev,
+             st_multi(st_union (jur.shoreline)) AS geom
+      FROM common_jurisdiction AS jur
+        JOIN common_lake AS lake ON lake.id = jur.lake_id
+      GROUP BY abbrev) AS shoreline
+WHERE common_lake.abbrev = shoreline.abbrev;
+
+-- update our lake centroid too:
+UPDATE common_lake
+   SET centroid = st_centroid(shoreline)
+WHERE shoreline IS NOT NULL;
+
+COMMIT;
---
+++ utils/shp2geojson.py
@@ -0,0 +1,161 @@
+"""This script is used to populate the spatial geometries for the
+Great Lakes Fish stocking dataviz application.
+
+Management units are currently read in from several shapefiles from
+the great GIS.  Split appart into seperate files, reprojected to
+lat-lon and then imported into postgis. Once in post gis, the
+managementunits, jurisdictions and lakes are rebuilt.
+
+This script assumes that you have postgis correctly installed on your
+computer as well as the associated command-line utility shp2pgsql
+
+"""
+
+import os
+import psycopg2
+import shapefile
+
+#the location of our source shapefiles and associated output
+
+SHP_SRC = './shapefiles'
+SHP_TRG = './output'
+
+#pg connections parameters
+con_pars = {
+    'HOST': 'localhost',
+    'NAME': 'fsdviz',
+    'USER': 'cottrillad',
+    'PASSWORD': os.getenv('PGPASS', 'password')
+}
+
+pg_constring = """host='{HOST}' dbname='{NAME}' user='{USER}'
+                  password='{PASSWORD}'"""
+pg_constring = pg_constring.format(**con_pars)
+
+# The shapefiles that hold the polygons that represent our management
+# units: the label index indicates the field that will be used to name
+# the output files. It must match the label of the corresponding
+# management unit in the common_managementunit table.
+
+shapefiles = [
+    # Lake Michigan
+    dict(shpfile="stat_dist.shp", srid='26916', labelIndex=0),
+
+    # Lake Huron
+    dict(shpfile="lh_stat_dist_poly_IFR.shp", srid='4269', labelIndex=3),
+    dict(shpfile="qmas.shp", srid='26917', labelIndex=0),
+
+    # Lake Ontario
+    dict(shpfile="lo_stat_dist_poly_IFR.shp", srid='4269', labelIndex=3),
+
+    # Lake Superior
+    dict(shpfile="ls_stat_dist_poly_IFR.shp", srid='4269', labelIndex=3),
+
+    # Lake Erie
+    dict(shpfile="le_stat_dist_poly_IFR.shp", srid='4269', labelIndex=3)
+]
+
+# check for our output directory:
+if not os.path.isdir(SHP_TRG):
+    print("Target Directory (SHP_TRG={}) Does not exist!".format(SHP_TRG))
+
+# create a sql sub-directory if one does not exist.
+if not os.path.isdir(os.path.join(SHP_TRG, 'sql')):
+    os.mkdir(os.path.join(SHP_TRG, 'sql'))
+
+# an empyt list to hold our mangement unit labels
+manUnits = []
+
+for lake in shapefiles:
+
+    # shpfile = os.path.join(ROOT_DIR, 'shapefiles', lake.get('shpfile'))
+    shpfile = os.path.join(SHP_SRC, lake.get('shpfile'))
+    labelIndex = lake.get('labelIndex')
+    spatialRef = lake.get('srid')
+
+    sf = shapefile.Reader(shpfile)
+
+    # output our files with names that match our management unit labels
+    # (currently uppercase without hyphens or periods, but this might change)
+    for i, record in enumerate(sf.records()):
+        manUnit = record[labelIndex]
+        # this is a work-around (only qmas.shp has names with dashes
+        # and lower case letters!)
+        if lake.get('shpfile') is not 'qmas.shp':
+            manUnit = manUnit.upper().replace('-', '').replace('.', '')
+        manUnits.append(manUnit)
+        shpname = os.path.join(SHP_TRG, '{}.shp'.format(manUnit))
+        thisShape = sf.shapeRecord(i)
+        with shapefile.Writer(
+                shpname, shapeType=thisShape.shape.shapeType) as shpwtr:
+            shpwtr.fields = sf.fields
+            shpwtr.shape(thisShape.shape)
+            shpwtr.record(*thisShape.record)
+
+    sf.close()
+
+    # now loop through our management units, using shp2pgsql to
+    # re-project them to WGS-84 (SRID=4623) in a field called
+    # geom. The sql will be written into a file called {MU}.sql That
+    # file is then executed by postgis to create a temporary table
+    # that will be used to update the geom feild of the associated
+    # record in common_managementunit table.  The names of the
+    # mangement in the postgis table must match the names of the
+    # sql_file!
+
+    cmd = ('shp2pgsql -s {SRID}:4326 -g "geom" "{source}" > "{target}"')
+
+    for mu in manUnits:
+        src_file = os.path.join(SHP_TRG, '{}.shp'.format(mu))
+        trg_file = os.path.join(SHP_TRG, 'sql/{}.sql'.format(mu))
+        vals = dict(SRID=spatialRef, source=src_file, target=trg_file)
+        os.system(cmd.format(**vals))
+    print("Done creating sql for mu polygons.")
+
+    # now connect to our database, import the geometries we just created,
+    # update the geom field of the associated managementunit record and
+    # deleted the temporary table.
+
+    pg_conn = psycopg2.connect(pg_constring)
+    pg_cur = pg_conn.cursor()
+
+    for mu in manUnits:
+        fname = os.path.join(SHP_TRG, 'sql/{}.sql'.format(mu))
+
+        with open(fname, 'r') as sql_file:
+            pg_cur.execute(sql_file.read())
+        pg_conn.commit()
+
+        # the update query:
+        sql = '''update common_managementunit set geom =(select geom from "{}")
+        where label=%s;'''
+        pg_cur.execute(sql.format(mu.lower()), (mu, ))
+
+        # clean up our temporary table:
+        sql = 'drop table "{}";'.format(mu.lower())
+        pg_cur.execute(sql)
+        pg_conn.commit()
+    pg_conn.close()
+
+print('Done updating mu geoms for;')
+
+# The sql script "Create_JurisdictionLake_Geometries.sql" constains a
+# series sql statements that are exectuted by PostGis to:
+#
+# - create all of the jurisdictions from their memeber management
+# units
+#
+# - create all of the lakes from thier member juristictions.
+#
+# - update all of the fields that contain centroids or bounding boxes
+#  of other spatial fields.
+
+pg_conn = psycopg2.connect(pg_constring)
+pg_cur = pg_conn.cursor()
+
+fname = "Create_JurisdictionLake_Geometries.sql"
+with open(fname, 'r') as sql_file:
+    pg_cur.execute(sql_file.read())
+pg_conn.commit()
+pg_conn.close()
+print("Done updating all spatial geometries.")
