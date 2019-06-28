de23afb620d1287eccc462c0b8b79af150d7a689
Adam Cottrill <adam.cottrill@ontario.ca>
2019-06-28 11:02:08 -0400

    Forgot to add new js files from last commit
    The last commit moved concordance arrays for marks, clips, tags and
    colours to their own js files.  Those files were missed in that
    commit. This commit adds them in.

---
+++ fsdviz/static/js/speciesColors.js
@@ -0,0 +1,52 @@
+// colours and species abbeviations used throught the application.
+// the colours are re-used in other plots, but will be assigned in
+// order of appearance.  Species always match based on the order of
+// these two arrays (LAT will always be  "#3cb44b",)
+
+// 19 colours
+const speciesColours = [
+  "#3cb44b",
+  "#4363d8",
+  "#e6194b",
+  "#f58231",
+  "#46f0f0",
+  "#ffe119",
+  "#f032e6",
+  "#911eb4",
+  "#bcf60c",
+  "#fabebe",
+  "#008080",
+  "#e6beff",
+  "#9a6324",
+  "#fffac8",
+  "#800000",
+  "#aaffc3",
+  "#808000",
+  "#ffd8b1",
+  "#000075",
+  "#808080",
+  "#ffffff"
+];
+
+//19 species
+const all_species = [
+  "LAT",
+  "CHS",
+  "RBT",
+  "BNT",
+  "COS",
+  "WAL",
+  "LTX",
+  "ATS",
+  "SPE",
+  "BKT",
+  "SOS",
+  "YEP",
+  "LHR",
+  "BLO",
+  "LAS",
+  "MUE",
+  "SMB",
+  "TIM",
+  "NOP"
+];
