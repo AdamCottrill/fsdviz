cf0d27e9612e890ddba1fc59563daefdb5984ca0
Cottrill <adam.cottrill@ontario.ca>
2020-11-27 15:08:41 -0500

    Preparing to push to github
    This commit includes a number of updates that will be needed for
    production.  Some are associated with js files built by rollup, some
    are changes/updates to python source files.

---
+++ fsdviz/common/choices.py
@@ -0,0 +1,21 @@
+"""
+=============================================================
+~/fsdviz/fsdviz/common/choices.py
+ Created: 29 Jul 2020 13:53:53
+
+ DESCRIPTION:
+
+
+ A. Cottrill
+=============================================================
+"""
+
+
+LATLON_FLAGS = [
+    (1, "Reported"),
+    (2, "Derived from place name"),
+    (3, "Derived from centroid 5-minute grid"),
+    (4, "Derived from centroid 10-minute grid"),
+    (5, "Derived from centroid management unit"),
+    (6, "Derived from centroid of juristiction"),
+]
--- fsdviz/common/templatetags/fsdviz_tags.py
+++ fsdviz/common/templatetags/fsdviz_tags.py
@@ -9,30 +9,55 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='format_cwt')
+@register.filter(name="filter_colour")
+def filter_colour(x):
+    """returns a colour used to fill-in buttons associated with specific
+    filters - ensures consistency across views and templates.
+
+    """
+
+    button_colours = {
+        "agency": "red",
+        "lake": "blue",
+        "year": "teal",
+        "first_year": "green",
+        "last_year": "olive",
+        "contains": "pink",
+        "species": "orange",
+        "strain": "yellow",
+        "lifestage": "violet",
+        "stocking_method": "brown",
+        # still have grey, black, and purple
+    }
+
+    return button_colours.get(x)
+
+
+@register.filter(name="format_cwt")
 def format_cwt(x):
-    '''
+    """
     format cwts as 63-03-99
-    '''
+    """
     x = str(x)
     cwt = "-".join([x[:2], x[2:4], x[4:6]])
     return cwt
 
 
-@register.filter(name='times')
+@register.filter(name="times")
 def times(number):
-    '''provides a range() like template filter to iterate over integer
+    """provides a range() like template filter to iterate over integer
     values.
 
     from :https://stackoverflow.com/questions/1107737
 
-    '''
+    """
 
-    return range(1, number+1)
+    return range(1, number + 1)
+
 
 @register.simple_tag(takes_context=True)
 def query_transform(context, include_page=False, **kwargs):
-    '''
+    """
     Returns the URL-encoded querystring for the current page,
     updating the params with the key/value pairs passed to the tag.
 
@@ -52,37 +77,37 @@ def query_transform(context, include_page=False, **kwargs):
     'refinement' widgets.  Without, refinement widgets may point to a
     page that doesn't exist after the new filter has been applied.
 
-    '''
+    """
 
-    query = context['request'].GET.copy()
+    query = context["request"].GET.copy()
     for k, v in kwargs.items():
-            query[k] = v
+        query[k] = v
 
-    if query.get('page') and not include_page:
-        query.pop('page')
+    if query.get("page") and not include_page:
+        query.pop("page")
     return query.urlencode()
 
 
 @register.simple_tag(takes_context=True)
 def strip_parameter(context, param):
-    '''
+    """
     A template tag to remove the specified parameter from the url
     string.  If there are no parameter left, it returns the bare
     url (without any parameters or ?-mark)
-    '''
+    """
 
-    query = context['request'].GET.copy()
+    query = context["request"].GET.copy()
     query.pop(param)
 
     if len(query):
-        return '?' + query.urlencode()
+        return "?" + query.urlencode()
     else:
-        return context['request'].path
+        return context["request"].path
 
 
 @register.simple_tag(takes_context=True)
 def next_year(context, year):
-    '''Used to incrcrement urls that contain year with the value for the
+    """Used to incrcrement urls that contain year with the value for the
     next year.  Parameters that are pass in are preserved and passed
     through to the destination url.  This tag is used in the event
     list templates.
@@ -93,22 +118,22 @@ def next_year(context, year):
 
     /stocking/events/HU/2009/?species=LAT
 
-    '''
+    """
 
-    query = context['request'].GET.copy()
+    query = context["request"].GET.copy()
 
     # conert to strings with leading slash
-    this_year = '/{}'.format(year)
-    next_year = '/{}'.format(year + 1)
-    mypath = context['request'].path.replace(this_year, next_year)
+    this_year = "/{}".format(year)
+    next_year = "/{}".format(year + 1)
+    mypath = context["request"].path.replace(this_year, next_year)
     if query:
-        mypath += '?' + query.urlencode()
+        mypath += "?" + query.urlencode()
     return mypath
 
 
 @register.simple_tag(takes_context=True)
 def last_year(context, year):
-    '''Used to increment urls that contain year with the previous year
+    """Used to increment urls that contain year with the previous year
     value. Parameters that are pass in are preserved and passed
     through to the destination url.  This tag is used in the event
     list templates.
@@ -119,15 +144,15 @@ def last_year(context, year):
 
     /stocking/events/HU/2007/?species=LAT
 
-    '''
+    """
 
-    query = context['request'].GET.copy()
+    query = context["request"].GET.copy()
 
     # conert to strings with leading slash
-    this_year = '/{}'.format(year)
-    last_year = '/{}'.format(year - 1)
-    mypath = context['request'].path.replace(this_year, last_year)
+    this_year = "/{}".format(year)
+    last_year = "/{}".format(year - 1)
+    mypath = context["request"].path.replace(this_year, last_year)
 
     if query:
-        mypath += '?' + query.urlencode()
+        mypath += "?" + query.urlencode()
     return mypath
