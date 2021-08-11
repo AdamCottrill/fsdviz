dc051c7b2cbae7cda3156f5ecc968210336b0262
Adam Cottrill <adam.cottrill@ontario.ca>
2021-08-11 17:30:37 -0400

    Added view and template for lookup tables
    This commit includes a view and html template that includes tables with
    the values for most of the look-up tables in the application.  Species,
    strains, life stages, stocking methods, clips, ect.

--- fsdviz/common/urls.py
+++ fsdviz/common/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import spatial_lookup
+from .views import lookup_tables, spatial_lookup
 
 app_name = "common"
 
-urlpatterns = [path("spatial_lookup/", spatial_lookup, name="spatial-lookup")]
+urlpatterns = [
+    path("spatial_lookup/", spatial_lookup, name="spatial-lookup"),
+    path("lookup_tables/", lookup_tables, name="lookup-tables"),
+]
--- fsdviz/common/views.py
+++ fsdviz/common/views.py
@@ -1,7 +1,22 @@
 from django.contrib.gis.db.models import Extent
 from django.shortcuts import render
+from fsdviz.stocking.models import (
+    Hatchery,
+    LifeStage,
+    StockingMethod,
+    YearlingEquivalent,
+)
 
-from .models import Lake
+from .models import (
+    Agency,
+    CompositeFinClip,
+    FishTag,
+    Jurisdiction,
+    Lake,
+    PhysChemMark,
+    Species,
+    StrainRaw,
+)
 
 
 def spatial_lookup(request):
@@ -13,4 +28,53 @@ def spatial_lookup(request):
         request,
         "common/spatial_lookup.html",
         {"map_bounds": map_bounds["geom__extent"]},
+    )
+
+
+def lookup_tables(request):
+    """A view to render tables with all of the lookup values."""
+
+    # NOTE - defer spatial fields!
+
+    # Management Units
+    # Strains and Raw Strains
+
+    agencies = Agency.objects.all()
+    lakes = Lake.objects.all()
+    jurisdictions = Jurisdiction.objects.all()
+    species = Species.objects.all()
+    strains = (
+        StrainRaw.objects.select_related("species")
+        .prefetch_related("strain", "strain__species")
+        .all()
+    )
+    clips = CompositeFinClip.objects.all()
+    fishtags = FishTag.objects.all()
+    physchemmarks = PhysChemMark.objects.all()
+    hatcheries = Hatchery.objects.all()
+    lifestages = LifeStage.objects.all()
+    yreq = (
+        YearlingEquivalent.objects.select_related("species", "lifestage")
+        .order_by("species__common_name", "yreq_factor")
+        .all()
+    )
+    stocking_methods = StockingMethod.objects.all()
+
+    return render(
+        request,
+        "common/lookup_tables.html",
+        {
+            "agencies": agencies,
+            "lakes": lakes,
+            "jurisdictions": jurisdictions,
+            "species": species,
+            "strains": strains,
+            "clips": clips,
+            "fishtags": fishtags,
+            "physchemmarks": physchemmarks,
+            "hatcheries": hatcheries,
+            "lifestages": lifestages,
+            "yreq": yreq,
+            "stocking_methods": stocking_methods,
+        },
     )
